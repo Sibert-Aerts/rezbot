f87337c7ec03ee785139c522e88ebadca8419b74
Sibert Aerts <sibert.aerts@gmail.com>
2020-04-26 12:27:42 +0200

    Add math
    This is the power of math, people

--- src/pipes/pipes.py
+++ src/pipes/pipes.py
@@ -11,6 +11,7 @@ from datamuse import datamuse
 datamuse_api = datamuse.Datamuse()
 from google.cloud import translate
 import nltk
+from simpleeval import SimpleEval
 
 from .signature import Sig
 from .pipe import Pipe, Pipes
@@ -596,3 +597,87 @@ def rot_pipe(text, by):
             c = chr( 65 + ( o - 65 + by ) % 26 )
         out.append(c)
     return ''.join(out)
+
+
+#####################################################
+#                    Pipes : MATH                   #
+#####################################################
+_CATEGORY = 'MATH'
+
+def smart_format(x: float):
+    x = str(x)
+    return re.sub('\.?0+$', '', x) if '.' in x else x
+
+MATH_FUNCTIONS = {
+    # Trigonometry
+    'sin': math.sin,
+    'cos': math.cos,
+    'tan': math.tan,
+    'atan': math.atan,
+    'atan2': math.atan2,
+    # Exponentiation
+    'exp': math.exp,
+    'log': math.log,
+    'log10': math.log10,
+    'log2': math.log2,
+    # This asshole
+    'factorial': math.factorial,
+    # Squares and roots
+    'sqrt': math.sqrt,
+    'hypot': math.hypot,
+    # Reduction
+    'floor': math.floor,
+    'ceil': math.ceil,
+    'round': round,
+    'abs': abs,
+    'sign': lambda x : -1 if x < 0 else 1 if x > 0 else 0,
+    # Number theory
+    'gcd': math.gcd,
+    # Statistics
+    'min': min,
+    'max': max,
+    'sum': sum,
+    'avg': lambda *x : sum(x)/len(x)
+}
+
+SIMPLE_EVAL = SimpleEval(functions=MATH_FUNCTIONS, names={'e': math.e, 'pi': math.pi, 'inf': math.inf, 'True': True, 'False': False})
+
+@make_pipe({
+    'expr': Sig(str, None, 'The mathematical expression to evaluate. Use {} notation to insert items into the expression.')
+}, command=True)
+@util.format_doc(funcs=', '.join(c for c in MATH_FUNCTIONS))
+def math_pipe(values, expr):
+    '''
+    Evaluates the mathematical expression given by the argument string.
+    
+    Available functions: {funcs}
+    Available constants: True, False, e, pi and inf
+    
+    Note: For finding the min, max, sum or avg of an arbitrary number of arguments, use the respective min, max, sum and avg pipes
+    '''
+    return [ smart_format(SIMPLE_EVAL.eval(expr)) ]
+        
+
+
+@make_pipe({})
+def max_pipe(values):
+    ''' Returns the maximum value of the inputs evaluated as numbers. '''
+    return [smart_format(max(float(x) for x in values))]
+
+
+@make_pipe({})
+def min_pipe(values):
+    ''' Returns the minimum value of the inputs evaluated as numbers. '''
+    return [smart_format(min(float(x) for x in values))]
+
+
+@make_pipe({})
+def sum_pipe(values):
+    ''' Returns the sum of the inputs evaluated as numbers. '''
+    return [smart_format(sum(float(x) for x in values))]
+
+
+@make_pipe({})
+def avg_pipe(values):
+    ''' Returns the mean average of the inputs evaluated as numbers. '''
+    return [smart_format(sum(float(x) for x in values)/len(values))]
