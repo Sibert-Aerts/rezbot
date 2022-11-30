cb5aebd6225034257466e3d11e77cc9af87bd517
Sibert Aerts <sibert.aerts@gmail.com>
2022-11-30 18:26:45 +0100

    Move slash commands to dedicated file

--- src/pipes/macrocommands.py
+++ src/pipes/macrocommands.py
@@ -43,30 +43,6 @@ typedict: dict[str, tuple[Macros, bool, Pipes, Callable]] = {
 }
 typedict_options = ', '.join('"' + t + '"' for t in typedict)
 
-async def autocomplete_macro(interaction: Interaction, name: str):
-    name = name.lower()
-    results = []
-    for macro in itertools.chain(pipe_macros.values(), source_macros.values()):
-        if name in macro.name:
-            choice_name = f"{macro.name} ({macro.kind})"
-            value = macro.name + " " + macro.kind.lower()
-            results.append(Choice(name=choice_name, value=value))
-            if len(results) >= 25:
-                break
-    return results
-
-def autocomplete_bound_macro(macros):
-    async def _autocomplete_bound_macro(interaction: Interaction, name: str):
-        name = name.lower()
-        results = []
-        for macro in macros.values():
-            if name in macro.name:
-                results.append(Choice(name=macro.name, value=macro.name))
-                if len(results) >= 25:
-                    break
-        return results
-    return _autocomplete_bound_macro
-
 
 class MacroCommands(MyCommands):
     FORCE_MACRO_CACHE = None
@@ -244,163 +220,6 @@ class MacroCommands(MyCommands):
         await ctx.send('Removed signature "{}" from {} {}'.format(signame, what, name))
 
 
-    # ========================== Macro Management (app-commands) ==========================
-
-    macro_group = app_commands.Group(name='macro', description='Define, redefine, describe or delete a macro')
-    ''' The `/macro <action>` command group'''
-
-    @macro_group.command(name='define')
-    @app_commands.describe(
-        macro_type="The type of macro",
-        name="The name of the macro",
-        code="The code to define the macro as",
-        description="The macro's description",
-        hidden="Whether the macro should show up in the general macro list",
-        force="Force the macro to save even if there are errors"
-    )
-    @app_commands.rename(macro_type="type")
-    async def macro_define(self, interaction: Interaction,
-        macro_type: Literal['pipe', 'source'],
-        name: str,
-        code: str,
-        description: str=None,
-        hidden: bool=False,
-        force: bool=False
-    ):
-        ''' Define a new macro. '''
-        author = interaction.user
-        macros, _, natives, check = typedict[macro_type]
-        
-        def reply(text: str, **kwargs):
-            return interaction.response.send_message(text, **kwargs)
-
-        name = name.split(' ')[0].lower()
-        if name in natives or name in macros:
-            await reply(f'A {macro_type} called `{name}` already exists, try the `/redefine` command.')
-            return
-
-        if not force and not await check(code, reply):
-            await interaction.channel.send('Run the command again with `force: True` to save it anyway.')
-            return
-
-        macro = Macro(macros.kind, name, code, author.name, author.id, desc=description, visible=not hidden)
-        macros[name] = macro
-        await reply(f'Successfully defined a new {macro_type} macro.', embed=macro.embed(interaction))
-
-    @macro_group.command(name='edit')
-    @app_commands.describe(
-        macro="The macro to edit",
-        code="If given, the macro's new code",
-        description="If given, the macro's new description",
-        hidden="If given, the macro's new visibility",
-        force="Force the macro to save even if there are errors"
-    )
-    @app_commands.autocomplete(macro=autocomplete_macro)
-    async def macro_edit(self, interaction: Interaction,
-        macro: str,
-        code: str=None,
-        description: str=None,
-        hidden: bool=None,
-        force: bool=None
-    ):
-        ''' Redefine one or more fields on an existing macro. '''
-        author = interaction.user
-        name, macro_type = macro.split(' ')
-        macros, _, natives, check = typedict[macro_type]
-
-        def reply(text: str, **kwargs):
-            return interaction.response.send_message(text, **kwargs)
-
-        name = name.split(' ')[0].lower()
-        if name not in macros:
-            await reply(f'A {macro_type} macro by that name was not found.')
-            return
-        macro = macros[name]
-
-        if not macro.authorised(author):
-            await reply('You are not authorised to modify that macro. Try defining a new one instead.')
-            return
-
-        if not force and code and not await check(code, reply):
-            await interaction.channel.send('Run the command again with `force: True` to save it anyway.')
-            return
-
-        if code is not None:
-            macro.code = code
-        if description is not None:
-            macro.desc = description
-        if hidden is not None:
-            macro.visible = not hidden
-
-        macros.write()
-        await reply(f'Successfully edited the {macro_type} macro.', embed=macro.embed(interaction))
-
-    @macro_group.command(name='delete')
-    @app_commands.describe(macro="The macro to delete")
-    @app_commands.autocomplete(macro=autocomplete_macro)
-    async def macro_delete(self, interaction: Interaction, macro: str):
-        ''' Delete a macro. '''
-        author = interaction.user
-        name, macro_type = macro.split(' ')
-        macros, *_ = typedict[macro_type]
-
-        def reply(text: str, **kwargs):
-            return interaction.response.send_message(text, **kwargs)
-
-        name = name.split(' ')[0].lower()
-        if name not in macros:
-            await reply(f'A {macro_type} macro by that name was not found.')
-            return
-        macro = macros[name]
-
-        if not macro.authorised(author):
-            await reply('You are not authorised to modify that macro.')
-            return
-
-        del macros[name]
-        await reply(f'Successfully deleted {macro_type} macro `{name}`.')
-
-    @macro_group.command(name='set_param')
-    @app_commands.describe(
-        macro="The macro",
-        param="The name of the parameter to assign",
-        description="The parameter's description to assign",
-        default="The parameter's default value to assign",
-    )
-    @app_commands.autocomplete(macro=autocomplete_macro)
-    async def macro_edit(self, interaction: Interaction,
-        macro: str,
-        param: str,
-        description: str=None,
-        default: str=None,
-    ):
-        ''' Add or overwrite a parameter on a macro. '''
-        author = interaction.user
-        name, macro_type = macro.split(' ')
-        macros, *_ = typedict[macro_type]
-
-        def reply(text: str, **kwargs):
-            return interaction.response.send_message(text, **kwargs)
-
-        name = name.split(' ')[0].lower()
-        if name not in macros:
-            await reply(f'A {macro_type} macro by that name was not found.')
-            return
-        macro = macros[name]
-
-        if not macro.authorised(author):
-            await reply('You are not authorised to modify that macro.')
-            return
-
-        is_overwrite = (param in macro.signature)
-        par = MacroSig(param, default, description)
-        macro.signature[param] = par
-        macros.write()
-
-        verbed = "overwrote" if is_overwrite else "added"
-        await reply(f'Successfully {verbed} parameter `{param}`.', embed=macro.embed(interaction))
-
-
     # ========================== Macro listing ==========================
 
     async def _macros(self, ctx, what, name):
@@ -454,7 +273,6 @@ class MacroCommands(MyCommands):
             
             for block in texttools.block_chunk_lines(infos): await ctx.send(block)
 
-
     @commands.command(name='pipe_macros', aliases=['pipe_macro', 'macro_pipes', 'macro_pipe'])
     async def pipe_macros_command(self, ctx, name=''):
         '''A list of all pipe macros, or details on a specific pipe macro.'''
@@ -465,71 +283,6 @@ class MacroCommands(MyCommands):
         '''A list of all source macros, or details on a specific source macro.'''
         await self._macros(ctx, 'source', name)
 
-
-    # ========================== Macro listing (app-commands) ==========================
-
-    async def _macros_app_command(self, interaction: Interaction, macros: Macros, hidden: bool, mine: bool):
-        '''Reply with a list of macros.'''
-        what = macros.kind.lower()
-        qualified_what = what
-
-        ## Filter based on the given name
-        filtered_macros = macros.hidden() if hidden else macros.visible()
-        if hidden:
-            qualified_what = 'hidden ' + what
-        if mine:
-            author = interaction.user
-            filtered_macros = [m for m in filtered_macros if int(macros[m].authorId) == author.id]
-            qualified_what = 'your ' + what
-
-        if not filtered_macros:
-            await interaction.response.send_message(f'No {qualified_what} macros found.')
-            return
-
-        ## Boilerplate
-        infos = []
-        infos.append(f'Here\'s a list of all {qualified_what} macros, use /{what}_macro [name] to see more info on a specific one.')
-        infos.append(f'Use /{what} for a list of native {qualified_what}s.')
-
-        ## Separate those with and without descriptions
-        desced_macros = [m for m in filtered_macros if macros[m].desc]
-        undesced_macros = [m for m in filtered_macros if not macros[m].desc]
-
-        ## Format the ones who have a description as a nice two-column block
-        if desced_macros:
-            infos.append('')
-            colW = len(max(desced_macros, key=len)) + 2
-            for name in desced_macros:
-                macro = macros[name]
-                info = name +  ' ' * (colW-len(name))
-                desc = macro.desc.split('\n', 1)[0]
-                info += desc if len(desc) <= 80 else desc[:75] + '(...)'
-                infos.append(info)
-
-        ## Format the other ones as just a list
-        if undesced_macros:
-            infos.append('\nWithout descriptions:')
-            infos += texttools.line_chunk_list(undesced_macros)
-        
-        first = True
-        for block in texttools.block_chunk_lines(infos):
-            if first:
-                await interaction.response.send_message(block)
-                first = False
-            else:
-                await interaction.channel.send(block)
-
-    @app_commands.command(name='pipe_macro')
-    @app_commands.describe(hidden='If true, shows (only) hidden macros', mine='If true, only shows your authored macros')
-    async def pipe_macros(self, interaction: Interaction, hidden: bool=False, mine: bool=False):
-        ''' List pipe macros. '''
-        await self._macros_app_command(interaction, pipe_macros, hidden, mine)
-
-    @app_commands.command(name='source_macro')
-    @app_commands.describe(hidden='If true, shows (only) hidden macros', mine='If true, only shows your authored macros')
-    async def source_macros(self, interaction: Interaction, hidden: bool=False, mine: bool=False):
-        ''' List source macros. '''
-        await self._macros_app_command(interaction, source_macros, hidden, mine)
 
     # ========================== Dumping macros ==========================
 
