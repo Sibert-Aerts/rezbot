0ba766d210dc8d6ef7864bdbbc81797c250c8423
Sibert Aerts <sibert.aerts@gmail.com>
2023-06-25 15:07:45 +0200

    Make `disable_event` spout contextual

--- src/pipes/implementations/spouts_meta.py
+++ src/pipes/implementations/spouts_meta.py
@@ -1,5 +1,6 @@
 from .spouts import spout_from_func, set_category, Par, Context
 from pipes.events import events
+from pipes.context import ContextError
 
 
 #####################################################
@@ -8,13 +9,17 @@ from pipes.events import events
 set_category('META')
 
 @spout_from_func({
-    # TODO: Default: ctx.event!
-    'name': Par(str, None, 'The name of the event to be disabled.')
+    'name': Par(str, None, 'The name of the event to be disabled, empty to disable the triggering event.', required=False)
 })
-async def disable_event_spout(ctx: Context, values, name):
-    ''' Disables the specified event. '''
-    if name not in events:
-        raise ValueError('Event %s does not exist!' % name)
-    event = events[name]
+async def disable_event_spout(ctx: Context, values, *, name):
+    ''' Disables the specified event, or the current script's triggering event. '''
+    if not name:
+        event = ctx.origin.event
+        if not event:
+            raise ContextError('Current script was not triggered by an Event.')
+    else:
+        if name not in events:
+            raise ValueError(f'Event "{name}" does not exist!')
+        event = events[name]
     if ctx.channel.id in event.channels:
         event.channels.remove(ctx.channel.id)
