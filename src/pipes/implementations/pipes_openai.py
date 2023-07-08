450db654206282a69e0d9b913f491553cadb004b
Sibert Aerts <sibert.aerts@gmail.com>
2023-07-08 21:51:14 +0200

    Add `gpt_chat` pipe for new OpenAI API

--- src/pipes/implementations/pipes_openai.py
+++ src/pipes/implementations/pipes_openai.py
@@ -43,10 +43,10 @@ class PipeGPTComplete:
         return permissions.has(user.id, permissions.trusted)
 
     @with_signature(
+        model             = Par(str, 'ada', 'The GPT model to use, generally: ada/babbage/curie/davinci.'),
         n                 = Par(int, 1, 'The number of completions to generate.', check=lambda n: n <= 10),
         max_tokens        = Par(int, 50, 'The limit of tokens to generate per completion, does not include prompt.'),
         temperature       = Par(float, .7, 'Value between 0 and 2 determining how creative/unhinged the generation is.'),
-        model             = Par(str, 'ada', 'The GPT model to use, generally: ada/babbage/curie/davinci.'),
         presence_penalty  = Par(float, 0, 'Value between -2 and 2, positive values discourage reusing already present words.'),
         frequency_penalty = Par(float, 0, 'Value between -2 and 2, positive values discourage reusing frequently used words.'),
         stop              = Par(str, None, 'String that, if generated, marks the immediate end of the completion.', required=False),
@@ -66,6 +66,60 @@ class PipeGPTComplete:
         if prepend_prompt:
             completions = [text + completion for completion in completions]
         return completions
+
+
+@pipe_from_class
+class PipeGPTChat:
+    name = 'gpt_chat'
+    command = True
+
+    def may_use(user: discord.User):
+        return permissions.has(user.id, permissions.trusted)
+
+    @with_signature(
+        system      = Par(str, None, 'The "system" chat prompt.', required=False),
+        user        = Par(str, None, 'An example "user" chat message.', required=False),
+        assistant   = Par(str, None, 'An example "assistant" chat response.', required=False),
+        # Kwargs
+        model             = Par(str, 'gpt-3.5-turbo', 'The GPT model to use, generally: gpt-3.5-turbo/gpt-4.'),
+        n                 = Par(int, 1, 'The number of completions to generate.', check=lambda n: n <= 10),
+        max_tokens        = Par(int, 50, 'The limit of tokens to generate per completion, does not include prompt.'),
+        temperature       = Par(float, .7, 'Value between 0 and 2 determining how creative/unhinged the generation is.'),
+        presence_penalty  = Par(float, 0, 'Value between -2 and 2, positive values discourage reusing already present words.'),
+        frequency_penalty = Par(float, 0, 'Value between -2 and 2, positive values discourage reusing frequently used words.'),
+        stop              = Par(str, None, 'String that, if generated, marks the immediate end of the completion.', required=False),
+    )
+    @staticmethod
+    def pipe_function(items, *, model, system, user, assistant, **kwargs):
+        '''
+        Generate a chat completion for the given input.
+        The given input is treated as a "user" message, and GPT generates an "assistant" response.
+        
+        You can fill out existing message history by using the system/user/assistant arguments.
+        The "system" message will authoritatively declare what the situation is and how the assistant should behave.
+        The "user" and "assistant" messages can be used to provide an example of the assistant's response to a user message.
+        '''
+        if not _is_openai_usable: return items
+
+        base_messages = []
+        if system:
+            base_messages.append({'role': 'system', 'content': system})
+        if user:
+            base_messages.append({'role': 'user', 'content': user})
+        if assistant:
+            base_messages.append({'role': 'assistant', 'content': assistant})
+
+        result = []
+        if items:
+            for item in items:
+                message = {'role': 'user', 'content': item}
+                response = openai.ChatCompletion.create(model=model, messages=(base_messages + [message]), **kwargs)
+                result.extend(choice.message.content for choice in response.choices)
+        else:
+            response = openai.ChatCompletion.create(model=model, messages=base_messages, **kwargs)
+            result.extend(choice.message.content for choice in response.choices)
+
+        return result
 
 
 @pipe_from_class
